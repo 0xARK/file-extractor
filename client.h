d66edc60a06730299ed21980f9cf61c5d5d9c602
0xARROWK <0xARROWK@users.noreply.github.com>
2024-02-28 15:24:12 +0100

    added forgiven client.h file

---
+++ client.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/inotify.h>
+
+#ifndef SERVER_CLIENT_H
+#define SERVER_CLIENT_H
+
+#define EVENT_SIZE      (sizeof(struct inotify_event))
+#define BUF_LEN         (1024 * (EVENT_SIZE + 16))
+
+void monitor_folder(char **folders, int folders_size, char* client_id);
+char* get_client_identifier();
+void transfer_file(char* filename, char* filepath, char* client_id);
+
+#endif //SERVER_CLIENT_H
